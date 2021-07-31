e7da667620cc9c1bc4e692516ea35654f157ee72
6mn12j <dbwk0404@hanmail.net>
2021-07-31 23:29:21 +0900

    ADD Home

---
+++ screens/Home.js
@@ -0,0 +1,71 @@
+import React, { useEffect } from 'react';
+import toDos from '../store';
+import { View, Text, Platform, Linking } from 'react-native';
+import styled from 'styled-components/native';
+//import { dbService } from '../firebase';
+//import { Alert } from 'react-native';
+import { Provider } from 'react-redux';
+//import * as Location from 'expo-location';
+import HomeContent from '../components/HomeContent';
+
+const ScheduleButton = styled.TouchableOpacity``;
+const ScheduleIcon = styled.Text``;
+
+//export default function Home({ navigation }) {
+export default function Home() {
+  //const goToScheduleToday = () => navigation.navigate('ScheduleToday');
+  //   const openAppSettings = () => {
+  //     if (Platform.OS === 'ios') {
+  //       Linking.openSettings();
+  //     } // android 일 때 생각해줘야함
+  //   };
+  //   const createTwoButtonAlert = () =>
+  //     Alert.alert(
+  //       '위치정보 이용 제한',
+  //       '설정에서 위치정보 이용에 대한 액세스 권한을 허용해주세요.',
+  //       [
+  //         {
+  //           text: '취소',
+  //           style: 'cancel',
+  //         },
+  //         { text: '설정', onPress: () => openAppSettings() },
+  //       ],
+  //       { cancelable: false }
+  //     );
+  //   useEffect(() => {
+  //     (async () => {
+  //       let { status } = await Location.requestForegroundPermissionsAsync();
+  //       if (status !== 'granted') {
+  //         createTwoButtonAlert();
+  //       } else {
+  //         const current = await Location.getCurrentPositionAsync({});
+  //         await dbService.collection('location').add({
+  //           latitude: current.coords.latitude,
+  //           longitude: current.coords.longitude,
+  //         });
+  //       }
+  //     })();
+  //   }, []);
+  return (
+    <>
+      <View
+        style={{
+          flex: 0.5,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: 20,
+        }}
+      >
+        <Text>{`오늘도
+하루를 치열하게`}</Text>
+        <ScheduleButton>
+          <ScheduleIcon>달력아이콘</ScheduleIcon>
+        </ScheduleButton>
+      </View>
+      <Provider store={toDos}>
+        <HomeContent />
+      </Provider>
+    </>
+  );
+}
---
+++ store.js
@@ -0,0 +1,44 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const toDos = createSlice({
+  name: 'toDoReducer',
+  initialState: [
+    {
+      id: '01',
+      title: '예시 테스트 1 자습',
+      starttime: '11:00',
+      finishtime: '12:00',
+      location: '스타벅스 어디점',
+      todo: ['영단어외우기', '커피마시기'],
+    },
+    {
+      id: '02',
+      title: '예시 테스트 2 운동',
+      starttime: '13:00',
+      finishtime: '15:00',
+      location: '어디학교 운동장',
+      todo: ['운동상체', '끝내주게숨쉬기'],
+    },
+  ],
+  reducers: {
+    create: (state, action) => {
+      console.log(action.payload);
+      const [starttime, finishtime, title, location = 'api로받아오기'] =
+        action.payload;
+      state.push({
+        id: Date.now(),
+        title,
+        starttime,
+        finishtime,
+        location,
+        todo: [],
+      });
+    },
+    add: (state, action) => {
+      state[state.length - 1].todo.push(action.payload);
+    },
+  },
+});
+
+export const { create, add } = toDos.actions;
+export default configureStore({ reducer: toDos.reducer });
