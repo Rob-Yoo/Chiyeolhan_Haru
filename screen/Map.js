d156b32b28ae803e66ae1c6efa8516f51add5692
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2021-08-01 14:52:40 +0900

    Modify googleapikey

--- screen/Map.js
+++ screen/Map.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, View, Dimensions } from 'react-native';
 
-export default function Map() {
+const Map = () => {
   return (
-    <View>
-      <Text>Map</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} provider={PROVIDER_GOOGLE}/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
+
+export default Map;
