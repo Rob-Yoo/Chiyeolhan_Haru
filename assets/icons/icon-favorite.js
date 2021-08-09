debb7b92f33f74c4ce11c71321d98712b79a033c
6mn12j <dbwk0404@hanmail.net>
2021-08-10 02:22:50 +0900

    ADD map icons

---
+++ assets/icons/icon-favorite.js
@@ -0,0 +1,4 @@
+import { createIconSetFromFontello } from 'react-native-vector-icons';
+import fontelloConfig from './icon-favorite.json';
+const Icon = createIconSetFromFontello(fontelloConfig, 'icon-favorite');
+export default Icon;
---
+++ assets/icons/icon-go-back-button.js
@@ -0,0 +1,4 @@
+import { createIconSetFromFontello } from 'react-native-vector-icons';
+import fontelloConfig from './icon-go-back-button.json';
+const Icon = createIconSetFromFontello(fontelloConfig, 'icon-go-back-button');
+export default Icon;
