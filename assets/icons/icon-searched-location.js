473e90859303834c74d3d8d8efba03e19f73a11d
6mn12j <dbwk0404@hanmail.net>
2021-08-22 04:28:38 +0900

    Chore: 검색기록 아이콘

---
+++ assets/icons/icon-searched-location.js
@@ -0,0 +1,7 @@
+import { createIconSetFromFontello } from 'react-native-vector-icons';
+import fontelloConfig from './icon-searched-location.json';
+const Icon = createIconSetFromFontello(
+  fontelloConfig,
+  'icon-searched-location',
+);
+export default Icon;
---
+++ assets/icons/icon-searched-search.js
@@ -0,0 +1,4 @@
+import { createIconSetFromFontello } from 'react-native-vector-icons';
+import fontelloConfig from './icon-searched-search.json';
+const Icon = createIconSetFromFontello(fontelloConfig, 'icon-searched-search');
+export default Icon;
