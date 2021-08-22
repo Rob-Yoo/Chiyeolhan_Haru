36e671f3d9fe5761d3a8271c070f589b437ca6b4
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2021-08-23 02:34:28 +0900

    Feat: 사용자의 도착 시간에 따른 푸쉬 알림 동작 구현

--- src/utils/Notification.js
+++ src/utils/Notification.js
@@ -4,9 +4,33 @@ export const successNotification = () => {
   PushNotification.localNotificationSchedule({
     //... You can use all the options from localNotifications
     id: '1',
-    message: '해당 장소에 도착하셨습니다. 당신의 치열한 하루를 응원합니다.', // (required)
-    date: new Date(Date.now() + 1000), // in 60 secs
+    message: '일정 장소 부근에 도착하셨네요. 당신의 치열한 하루를 응원합니다.', // (required)
+    date: new Date(Date.now() + 1000), // 120초로 나중에 바꾸자
     allowWhileIdle: false, // (optional) set notification to work while on doze, default: false
   });
   PushNotification.removeDeliveredNotifications(['1']);
+};
+
+export const arriveEarlyNotification = () => {
+  PushNotification.localNotificationSchedule({
+    //... You can use all the options from localNotifications
+    id: '2',
+    message:
+      '일정 장소 부근에 일찍 도착하셨네요. 부지런한 당신의 치열한 하루를 응원합니다.', // (required)
+    date: new Date(Date.now() + 1000), // in 60 secs
+    allowWhileIdle: false, // (optional) set notification to work while on doze, default: false
+  });
+  PushNotification.removeDeliveredNotifications(['2']);
+};
+
+export const arriveTooEarlyNotification = (time) => {
+  PushNotification.localNotificationSchedule({
+    //... You can use all the options from localNotifications
+    id: '3',
+    message:
+      '일정 장소 부근에 일찍 도착하셨네요. 부지런한 당신의 치열한 하루를 응원합니다.', // (required)
+    date: new Date(Date.now() + 1000 * (time * 60 - 600)), // in 60 secs
+    allowWhileIdle: false, // (optional) set notification to work while on doze, default: false
+  });
+  PushNotification.removeDeliveredNotifications(['3']);
 };
