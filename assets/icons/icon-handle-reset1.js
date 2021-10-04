a0bece354770d1e08c4d2b2b140313df2588b4c2
6mn12j <dbwk0404@hanmail.net>
2021-10-04 23:18:50 +0900

    Add: Icon

---
+++ assets/icons/icon-handle-reset1.js
@@ -0,0 +1,4 @@
+import { createIconSetFromFontello } from 'react-native-vector-icons';
+import fontelloConfig from './icon-handle-reset1.json';
+const Icon = createIconSetFromFontello(fontelloConfig, 'icon-handle-reset1');
+export default Icon;
---
+++ assets/icons/icon-handle-start.js
@@ -0,0 +1,4 @@
+import { createIconSetFromFontello } from 'react-native-vector-icons';
+import fontelloConfig from './icon-handle-start.json';
+const Icon = createIconSetFromFontello(fontelloConfig, 'icon-handle-start');
+export default Icon;
