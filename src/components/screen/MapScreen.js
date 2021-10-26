83bc5acd826e1940dca1c2801b2fa8b20439e56b
6mn12j <dbwk0404@hanmail.net>
2021-10-26 18:28:37 +0900

    Feat: 구글지도에서 애플지도로 변경

--- src/components/screen/MapScreen.js
+++ src/components/screen/MapScreen.js
@@ -5,7 +5,7 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { GOOGLE_PLACES_API_KEY } from '@env';
 
@@ -137,7 +137,6 @@ const CurrentMap = ({
 
   const _handlePlacesAPI = async (text) => {
     try {
-      // console.log(GOOGLE_PLACES_API_KEY);
       const place = text.replaceAll(' ', '%20');
       const response = await fetch(
         `${GOOGLE_API_URL}?input=${place}&${GOOGLE_PARARMS}&key=${GOOGLE_PLACES_API_KEY}`,
@@ -188,7 +187,6 @@ const CurrentMap = ({
       {/*MapView*/}
       <MapView
         style={styles.map}
-        //provider={PROVIDER_GOOGLE}
         userInterfaceStyle={'light'}
         region={{
           ...locationResult,
